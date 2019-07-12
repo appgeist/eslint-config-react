4fc0074ce57230a74bcdc4179cc6b01597e4113c
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2019-07-12 19:02:00 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# @appgeist/eslint-config-react
+
+[![NPM version][npm-image]][npm-url]
+[![License][license-image]][license-url]
+
+This package provides AppGeist's Next.js/React .eslintrc as an extensible shared config.
+
+## Usage
+
+Add `"extends": "@appgeist/eslint-config-react"` to your `.eslintrc.json`.
+
+## Default rules
+
+See [index.js](index.js).
+
+[npm-image]: https://img.shields.io/npm/v/@appgeist/eslint-config-react.svg?style=flat-square
+[npm-url]: https://www.npmjs.com/package/@appgeist/eslint-config-react
+[license-image]: https://img.shields.io/npm/l/@appgeist/eslint-config-react.svg?style=flat-square
+[license-url]: LICENSE
---
+++ index.js
@@ -0,0 +1,55 @@
+// prettier-ignore
+module.exports = {
+  root: true,
+  env: { es6: true, browser: true },
+  parser: 'babel-eslint',
+  parserOptions: { ecmaVersion: 10 },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'airbnb',
+    'prettier'
+  ],
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': { rootPathPrefix: '~', rootPathSuffix: '.' }
+    }
+  },
+  plugins: ['import', 'prettier'],
+  rules: {
+    'indent': 'error',
+    'max-len': ['warn', 120],
+    'comma-dangle': ['error', 'never'],
+    'prefer-const': 'warn',
+    'camelcase': 'off',
+    'no-nested-ternary': 'off',
+    'no-restricted-globals': 'off',
+    'no-plusplus': 'off',
+    'no-param-reassign': ['error', { props: false }],
+    'arrow-parens': ['error', 'as-needed', { requireForBlockBody: true }],
+    'function-paren-newline': ['error', 'consistent'],
+    'space-in-parens': ['error', 'never'],
+    'object-curly-newline': ['error', { consistent: true, multiline: true }],
+    'object-curly-spacing': ['error', 'always'],
+    'semi': ['error', 'always'],
+    'key-spacing': 'error',
+    'comma-spacing': 'error',
+    'comma-style': 'error',
+    'space-infix-ops': 'error',
+    'global-require': 'off',
+    'radix': 'off',
+    'quotes': ['error', 'single', 'avoid-escape'],
+    'quote-props': ['error', 'consistent'],
+    'import/no-unresolved': ['error', { ignore: [ 'mdi-react' ] }],
+    'react/jsx-filename-extension': ['error', { extensions: ['.js'] }],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'react/jsx-closing-tag-location': 'off',
+    'react/destructuring-assignment': 'off',
+    'react/button-has-type': 'off',
+    'jsx-a11y/anchor-is-valid': ['error', { aspects: ['invalidHref', 'preferButton'] }],
+    'jsx-a11y/label-has-for': 'off',
+    'jsx-a11y/label-has-associated-control': 'off'
+  }
+};
